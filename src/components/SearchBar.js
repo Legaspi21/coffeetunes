6ec3fc98eee27441784bd64dbc85f813141a6b93
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-05-06 13:38:01 -0700

    Adds handlers for change and submission of search terms

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,44 @@
 import React, {Component} from 'react';
 import FaSearch from 'react-icons/lib/fa/search';
+import {connect} from 'react-redux';
+import {updateSearch, fetchEvents} from '../actions';
 import './SearchBar.css';
 
 class SearchBar extends Component {
-
+	handleChange(e){
+    const text = e.target.value;
+    const {updateSearch} = this.props;
+    updateSearch(text);
+  }
+  handleSubmit(e) {
+    const {fetchEvents,city} = this.props;
+    e.preventDefault();
+    fetchEvents(city);
+  }
   render() {
     return (
       <div className="search-bar">
-      	<FaSearch className="search-bar__icon"/>
-      	<input className="search-bar__input" type="search" id="search" placeholder="Search by city"/>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <FaSearch className="search-bar__icon"/>
+          <input 
+          className="search-bar__input" 
+          type="search" 
+          id="search" 
+          placeholder="Search by city"
+          onChange={(e) => this.handleChange(e)}
+          />
+        </form>
       </div>
     );
   }
 }
-
-export default SearchBar;
+function mapStateToProps(state) {
+  const {city} = state.data;
+    return {city};
+}
+export default connect(
+  mapStateToProps, {
+    updateSearch,
+    fetchEvents
+  }
+)(SearchBar);
