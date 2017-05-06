d4d94be8a1e786c7453947d405e28c6e29637b07
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-05-06 14:27:26 -0700

    More redux stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import { connect } from 'react-redux';
 import Header from './components/Header';
 import EventList from './components/EventList';
-import {TOKEN} from './Key';
+// import {TOKEN} from './Key';
+import {updateSearch,fetchEvents} from './actions';
 import './App.css';
 
 class App extends Component {
@@ -11,35 +13,48 @@ class App extends Component {
     city: 'New York'
   }
   componentWillMount() {
-    //TODO: Fetch Events from API
-    const self = this;
-    const {city} = this.state;
-    axios.get('https://www.eventbriteapi.com/v3/events/search/', {
-      params: {
-        token: TOKEN,
-        categories: '103',
-        'location.address': city,
-        'location.within': '25mi'
-      }
-    })
-    .then(function (response) {
-      self.setState({events:response.data.events});
-      // console.log(response.data.events);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    // //TODO: Fetch Events from API
+    // const self = this;
+    // const {city} = this.props.data;
+    // axios.get('https://www.eventbriteapi.com/v3/events/search/', {
+    //   params: {
+    //     token: TOKEN,
+    //     categories: '103',
+    //     'location.address': city,
+    //     'location.within': '25mi'
+    //   }
+    // })
+    // .then(function (response) {
+    //   self.setState({events:response.data.events});
+    //   // console.log(response.data.events);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
+    this.props.fetchEvents(this.props.data.city);
   }
+  // componentWillReceiveProps(nextProps) {
+  //   this.props.fetchEvents(nextProps.data.city);
+  // }
   render() {
-    let {events} = this.state;
-    console.log(events)
+    let {events} = this.props.data;
     return (
       <div className="App">
-        <Header title="Coffee Tunes" subtitle="Discover Artists - Meet for Coffee"/>
+        <Header title="Coffee Tunes" subtitle="Discover Artists - Meet for Coffee" />
         {events.length > 1 ? <EventList events={events} /> : null}
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  const {data} = state;
+    return {data};
+}
+
+export default connect(
+  mapStateToProps, {
+    updateSearch,
+    fetchEvents
+  }
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import App from './App';
 import './index.css';
 
+const store = configureStore();
+
+
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
