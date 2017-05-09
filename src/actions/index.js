7e187a6302a66c43715aea159e28e07004633577
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-05-08 23:29:56 -0700

    Refactor fetchEvents to add lat and long to event object utilizing promise based design pattern

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
-import {TOKEN} from '../Key';
+// import yelp from 'yelp-fusion';
+import {
+  TOKEN, 
+  PERSONAL_TOKEN, 
+  // YELP_OAUTH_TOKEN, 
+  // CLIENT_ID as clientId, 
+  // CLIENT_SECRET as clientSecret, 
+  // GOOGLE_API_KEY
+} from '../Key';
 
 const updateEvents = (dispatch, events) => {
   dispatch({
@@ -9,31 +17,101 @@ const updateEvents = (dispatch, events) => {
 };
 
 export const updateSearch = (text) => {
+  console.log(text)
   return {
     type: 'UPDATE_SEARCH',
     payload: text,
   }
 }
 
-
-
 export const fetchEvents = (city) => {
+  var eventArray = [];
   return (dispatch) => {
-  // dispatch({ type: UPDATE_EVENTS });
   axios.get('https://www.eventbriteapi.com/v3/events/search/', {
 	  params: {
 	    token: TOKEN,
 	    categories: '103',
 	    'location.address': city,
-	    'location.within': '25mi'
+	    'location.within': '5mi'
 	  }
 	})
-  .then(response => updateEvents(dispatch, response.data.events)
-    // console.log(response.data.events);
-  )
+  .then(response => {
+    const {events} = response.data;
+    events.forEach(event => {
+      let {venue_id} = event;
+      axios.get(`https://www.eventbriteapi.com/v3/venues/${venue_id}/`, {
+        params: {
+          token: PERSONAL_TOKEN
+        },
+      })
+      .then(response => {
+        event.latitude = response.data.latitude;
+        event.longitude = response.data.longitude;
+        eventArray.push(event)
+      })
+      .catch(function (error) {
+        console.log("error")
+        console.log(error);
+      });
+    })   
+    
+  })
+  .then(() => {
+    updateEvents(dispatch, eventArray)
+  })
   .catch(function (error) {
     console.log(error);
   });
   };
 };
 
+// TODO: Resolve CORS problem and use YELP API. Create proxy node server
+
+// const getNearestCoffeeShop = (latitude,longitude) => {
+  
+// // console.log(YELP_OAUTH_TOKEN(clientId,clientSecret))
+//   // const client = yelp.client(YELP_OAUTH_TOKEN,{mode: 'no-cors'});
+
+//   // client.search({
+//   //   term:'Coffee',
+//   //   limit: 1,
+//   //   location: location
+//   // }, {mode: 'no-cors'}).then(response => {
+//   //   console.log(response.jsonBody.businesses[0].name);
+//   // });
+//   // TODO: Resolve CORS problem and use GOOGLE PLACES API as a backup
+//   // axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
+//   //   params: {
+//   //     location:`${latitude},${longitude}`,
+//   //     radius:500,
+//   //     term:'Coffee',
+//   //     limit:1,
+//   //     key:GOOGLE_API_KEY
+//   //   },
+//   //   mode: 'no-cors',
+//   // })
+//   // .then(response => {
+//   //   console.log('YOOOOOOOOOOOOOOOOOOO')
+//   //   console.log(response.results);
+//   // })
+//   // .catch(error => {
+//   //   console.log(error);
+//   // })
+
+//   const coffee = require('nearest-coffee')
+//   coffee({ 
+//       location: [latitude, longitude], 
+//       radius: 100,
+//       limit: 1,
+//       rankBy: 'distance'
+//   }, (err, data) => {
+//       if (err) 
+//           throw err
+   
+//       let places = data.map((d) => {
+//           return d.name
+//       })
+   
+//       console.log(places)
+//   })
+// }
