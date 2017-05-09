736febda5b7865808553a70ab1151be51c9fce4a
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-05-09 16:25:43 -0700

    Adds call to action for event link

--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -26,9 +26,10 @@ class EventDetail extends Component {
 	}
 	render() {
 		let {image,name,url,is_free,latitude,longitude,date} = this.props;
-		let displayDate = new Date(date).toLocaleDateString('en-US',{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+		let displayDate = new Date(date).toLocaleDateString('en-US',{ year: 'numeric', month: 'long', day: 'numeric' });
 		let {placeLoaded} = this.state;
 		if (placeLoaded) {
+			console.log(this.state.place)
 			let {photos,vicinity} = this.state.place;
 		}
 		return(
@@ -37,21 +38,21 @@ class EventDetail extends Component {
 					<div className="event-detail__card_front">
 						<div className={`event-detail__is-free-${is_free ? 'box' : 'inactive'}`}><h3 className="event-detail__is-free">{is_free ? 'FREE' : null}</h3></div>
 						<img className="event-detail__image" src={image} alt=""/>
-						<a className="event-detail__link" href={url}><div className="event-detail__detail">
+						<a className="event-detail__link" href={url}><div className="event-detail__detail event-detail__cta">
 							<p className="event-detail__description">{`${name} - ${displayDate}`}</p>	
 						</div></a>
-						<div className="event-detail__coffee event-detail__detail" onClick={this.handleClick.bind(this)}>
-							<p className="event-detail__description">Meet Up Spot</p>
+						<div className="event-detail__coffee event-detail__detail event-detail__font-white" onClick={this.handleClick.bind(this)}>
+							<p className="event-detail__description">☕ Meet Up Spot ☕</p>
 						</div>
 					</div>
 					<div className="event-detail__card_back">
 						<img className="event-detail__image" src={placeLoaded ? this.state.place.photos[0].getUrl({'maxWidth': 1000, 'maxHeight': 1000}) : null} alt=""/>
-						<div className="event-detail__detail">
+						<div className="event-detail__detail event-detail__no-highlight">
 					{/*TODO: Link to a map */}
 							<p className="event-detail__description">{placeLoaded ? `${this.state.place.name} @ ${this.state.place.vicinity}` : null}</p>	
 						
 						</div>
-						<div className="event-detail__coffee event-detail__detail" onClick={this.handleClick.bind(this)}>
+						<div className="event-detail__coffee event-detail__detail event-detail__font-white" onClick={this.handleClick.bind(this)}>
 							<p className="event-detail__description">Back</p>
 						</div>
 					</div>
